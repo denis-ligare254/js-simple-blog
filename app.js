4de648f667daf7bab1494ebc8e3f4e7152a53586
denis <mrisholigare@gmail.com>
2020-12-09 14:41:15 +0300

    application completed

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const reviews = [
     id: 1,
     name: "susan smith",
     job: "web developer",
-    img: 
+    img: "https://images.hgmsites.net/lrg/lamborghini-huaracan_100730251_l.jpg",
        // "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-      "href='images/1.jpg'",
+     
     text:
       "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
@@ -82,3 +82,8 @@ prevbtn.addEventListener('click',function(){
  }
   showperson(currentItem)
 })
+// surprise me
+// const surprise =document.getElementById('random-btn');
+// surprise.addEventListener('click',function(){
+
+// })
